76243303d848983dccdd349e835d9fc748f0b2c4
Benhur <benhurbbs@gmail.com>
2022-06-21 22:51:11 -0300

    Solicitação de resgate de senha

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -4,7 +4,7 @@ import { el } from './elements'
 class Toast {
 
     shouldHaveText(expectedText) {
-        cy.get(el.toast)
+        cy.get(el.toast, { timeout: 10000 })
             .should('be.visible')
             .find('p')
             .should('have.text', expectedText)
