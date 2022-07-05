ea0f521a5a8a404eb325c2649c029a3d13eca56e
Benhur <benhurbbs@gmail.com>
2022-07-04 23:32:33 -0300

    Evoluindo o arquivo dash para teste da api

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -1,8 +1,8 @@
 
 
-describe('dashboard', function() {
-    context('quando o cliente faz um agendamento no app mobile', function() {
-        
+describe('dashboard', function () {
+    context('quando o cliente faz um agendamento no app mobile', function () {
+
         const data = {
             costumer: {
                 name: 'Nikki Sixx',
@@ -18,13 +18,33 @@ describe('dashboard', function() {
             }
         }
 
-        before(function() {
+        before(function () {
             cy.postUser(data.costumer)
             cy.postUser(data.samurai)
+
+            cy.apiLogin(data.costumer)
+            cy.log('conseguimos pegar o token ' + Cypress.env('apiToken'))
         })
 
-        it('o mesmo deve ser exibido no dashboard', function() {
+        it('o mesmo deve ser exibido no dashboard', function () {
             console.log(data)
         })
+    })
+})
+
+Cypress.Commands.add('apiLogin', function (user) {
+
+    const payload = {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/sessions',
+        body: payload
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+        Cypress.env('apiToken', response.body.token)
     })
 })
