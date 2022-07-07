1175b5db77b92ee5a67fb8a3c34ac77d15ed2d5d
Benhur <benhurbbs@gmail.com>
2022-07-07 20:23:47 -0300

    Simulando o agendamento no Mobile via API REST

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -1,4 +1,5 @@
 
+import loginPage from '../support/pages/login'
 
 describe('dashboard', function () {
     context('quando o cliente faz um agendamento no app mobile', function () {
@@ -26,13 +27,16 @@ describe('dashboard', function () {
             cy.log('conseguimos pegar o token ' + Cypress.env('apiToken'))
 
             cy.setProviderId(data.provider.email)
+            cy.createAppointment()
         })
 
         it('o mesmo deve ser exibido no dashboard', function () {
-            cy.log('Id do Ramon é ' + Cypress.env('providerId'))
-            console.log(data)
-
-            cy.createAppointment()
+            
+            loginPage.go()
+            loginPage.form(data.provider)
+            loginPage.submit()
+            
+            cy.wait(3000)            
         })
     })
 })
@@ -42,9 +46,25 @@ import moment from 'moment'
 Cypress.Commands.add('createAppointment', function () {
     let now = new Date()
     now.setDate(now.getDate() + 1)
-    // cy.log(now.getDate( ))
-    const day = moment(now).format('YYYY-MM-DD 14:00:00')
-    cy.log(day)
+    
+    const date = moment(now).format('YYYY-MM-DD 14:00:00')
+
+    const payload = {
+        provider_id: Cypress.env('providerId'),
+        date: date
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/appointments',
+        body: payload,
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+        Cypress.env('apiToken', response.body.token)
+    })
 })
 
 Cypress.Commands.add('setProviderId', function (providerEmail) {
