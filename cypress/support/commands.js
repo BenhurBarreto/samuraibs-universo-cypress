1f8ab4109b5cb4d7f57d332ffad748c55cb4e8ff
Benhur <benhurbbs@gmail.com>
2022-07-08 15:46:14 -0300

    API Server no arquivo de configuração do Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,28 +26,30 @@
 
 import moment from 'moment'
 
+import { apiServer } from '../../cypress.json'
+
 Cypress.Commands.add('postUser', function (user) {
     cy.task('removeUser', user.email)
         .then(function (result) {
             console.log(result)
         })
 
-    cy.request(
-        'POST',
-        'http://localhost:3333/users',
-        user
-    ).then(function (response) {
+    cy.request( {
+        method: 'POST',
+        url: apiServer + '/users',
+        body: user
+    }).then(function (response) {
         expect(response.status).to.eq(200)
     })
 })
 
 Cypress.Commands.add('recoveryPass', function (email) {
 
-    cy.request(
-        'POST',
-        'http://localhost:3333/password/forgot',
-        { email: email }
-    ).then(function (response) {
+    cy.request({
+        method: 'POST',
+        url: apiServer + '/password/forgot',
+        body: { email: email }
+    }).then(function (response) {
         expect(response.status).to.eq(204)
 
         cy.task('findToken', email)
@@ -74,7 +76,7 @@ Cypress.Commands.add('createAppointment', function (hour) {
 
     cy.request({
         method: 'POST',
-        url: 'http://localhost:3333/appointments',
+        url: apiServer + '/appointments',
         body: payload,
         headers: {
             authorization: 'Bearer ' + Cypress.env('apiToken')
@@ -89,7 +91,7 @@ Cypress.Commands.add('setProviderId', function (providerEmail) {
 
     cy.request({
         method: 'GET',
-        url: 'http://localhost:3333/providers',
+        url: apiServer + '/providers',
         headers: {
             authorization: 'Bearer ' + Cypress.env('apiToken')
         }
@@ -116,7 +118,7 @@ Cypress.Commands.add('apiLogin', function (user) {
 
     cy.request({
         method: 'POST',
-        url: 'http://localhost:3333/sessions',
+        url: apiServer + '/sessions',
         body: payload
     }).then(function (response) {
         expect(response.status).to.eq(200)
