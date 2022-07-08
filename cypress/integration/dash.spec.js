3bb1f7b61a1ee061e9100210f1289a2058930a40
Benhur <benhurbbs@gmail.com>
2022-07-08 15:17:39 -0300

    Fixtures? Que tal usar Factories em Javascript?

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -2,39 +2,25 @@
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
 
+import { customer, provider, appointment } from '../support/factories/dash'
+
 describe('dashboard', function () {
     context('quando o cliente faz um agendamento no app mobile', function () {
 
-        const data = {
-            costumer: {
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycrue.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramon@televisa.com',
-                password: 'pwd123',
-                is_provider: true
-            },
-            appointmentHour: '14:00'
-        }
-
         before(function () {
-            cy.postUser(data.provider)
-            cy.postUser(data.costumer)
+            cy.postUser(provider)
+            cy.postUser(customer)
 
-            cy.apiLogin(data.costumer)
+            cy.apiLogin(customer)
 
-            cy.setProviderId(data.provider.email)
-            cy.createAppointment(data.appointmentHour)
+            cy.setProviderId(provider.email)
+            cy.createAppointment(appointment.hour)
         })
 
         it('o mesmo deve ser exibido no dashboard', function () {
             
             loginPage.go()
-            loginPage.form(data.provider)
+            loginPage.form(provider)
             loginPage.submit()
 
             dashPage.calendarShouldBeVisible()
@@ -42,7 +28,7 @@ describe('dashboard', function () {
             const day = Cypress.env('appointmentDay')
             dashPage.selectDay(day)
 
-            dashPage.appointmentShouldBeVisible(data.costumer, data.appointmentHour)
+            dashPage.appointmentShouldBeVisible(customer, appointment.hour)
         })
     })
 })
